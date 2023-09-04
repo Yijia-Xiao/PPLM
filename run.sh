898ed61ebd37dd85276406e8e03d56274e37c235
Yijia-Xiao <yijia-xiao@outlook.com>
2023-09-04 16:37:19 -0400

    experiment scrips. 1 epoch for cord19. 5 for the rest.

--- run.sh
+++ run.sh
@@ -4,13 +4,17 @@
 set -x
 
 SIZE=7B
-EP=10
+EP=5
 BS=64
-ML=512
+ML=384
 
 # mediqa usmle_self_assessment mmmlu medical_flashcards wikidoc_patient_information wikidoc pubmed_causal medqa health_advice cord19;
-for SUBSET in medical_flashcards; # wikidoc;
+for SUBSET in cord19; # medical_flashcards; # wikidoc;
 do
+    if [ "$SUBSET" = "cord19" ]; then
+        EP=3
+    fi
+
     for STGY in contrast instruct;
     do
         TASK=instruct
@@ -18,30 +22,34 @@ do
         echo $LOCA
         mkdir -p ckpt/$LOCA
 
-        (torchrun --nnodes 1 --nproc_per_node 4  llama_finetuning.py \
+        torchrun --nnodes 1 --nproc_per_node 4  llama_finetuning.py \
             --enable_fsdp --use_peft --peft_method lora --model_name models_hf/$SIZE \
             --pure_bf16 --output_dir ckpt/$LOCA/ \
             --num_epochs $EP --batch_size_training $BS --micro_batch_size $BS \
-            --num_workers_dataloader 64 --use_fast_kernels --dataset ${TASK}_dataset --subset $SUBSET --maxlen $ML --inst_strategy $STGY) 2>&1 | tee -a ckpt/$LOCA/train.log
+            --num_workers_dataloader 64 --use_fast_kernels --dataset ${TASK}_dataset --subset $SUBSET --maxlen $ML --inst_strategy $STGY 2>&1 | tee -a ckpt/$LOCA/train.log
 
         python inference/hf-text-generation-inference/merge_lora_weights.py --base_model models_hf/7B --peft_model ckpt/$LOCA/ --output_dir ckpt/merge/$LOCA/
     done
 done
 
 
-for SUBSET in medical_flashcards; # wikidoc;
+for SUBSET in cord19; # medical_flashcard; # wikidoc;
 do
-    for TASK in original; # mask
+    if [ "$SUBSET" = "cord19" ]; then
+        EP=3
+    fi
+
+    for TASK in original mask;
     do
         LOCA=$TASK-$SUBSET-$SIZE-$ML-$BS
         echo $LOCA
         mkdir -p ckpt/$LOCA
 
-        (torchrun --nnodes 1 --nproc_per_node 4  llama_finetuning.py \
+        torchrun --nnodes 1 --nproc_per_node 4  llama_finetuning.py \
             --enable_fsdp --use_peft --peft_method lora --model_name models_hf/$SIZE \
             --pure_bf16 --output_dir ckpt/$LOCA/ \
             --num_epochs $EP --batch_size_training $BS --micro_batch_size $BS \
-            --num_workers_dataloader 64 --use_fast_kernels --dataset ${TASK}_dataset --subset $SUBSET --maxlen $ML) 2>&1 | tee -a ckpt/$LOCA/train.log
+            --num_workers_dataloader 64 --use_fast_kernels --dataset ${TASK}_dataset --subset $SUBSET --maxlen $ML 2>&1 | tee -a ckpt/$LOCA/train.log
 
         python inference/hf-text-generation-inference/merge_lora_weights.py --base_model models_hf/7B --peft_model ckpt/$LOCA/ --output_dir ckpt/merge/$LOCA/
     done
