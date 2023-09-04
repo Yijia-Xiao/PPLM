50e465bc84614f3164ba2b9e63d4670b5756974e
Yijia-Xiao <yijia-xiao@outlook.com>
2023-09-04 16:21:10 -0400

    Update template and inference scripts

--- ft_datasets/template.py
+++ ft_datasets/template.py
@@ -11,18 +11,29 @@ PROMPT_DICT = {
     ),
     "instruct_tuning_instruct": (
         "Below is a question and related responses. "
-        "Write \n(1) a response answering the question. \n(2) a privecy protection version of the response. \n\n"
+        "Write \n(1) a response answering the question. \n(2) a privacy protection version of the response. \n\n"
         "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
     ),
     "instruct_output_instruct": (
-        "(1) a response answering the question: {output}\n(2) a privecy protection version of the response: {cleaned_output}\n"
+        "(1) a response answering the question: {output}\n(2) a privacy protection version of the response: {cleaned_output}\n"
     ),
+    # "instruct_tuning_contrast": (
+    #     "Below is a question and related responses: a desired one and an undesired one. The undesired and desired examples will not be given during inference.\n\n"
+    #     "### Instruction:\n{instruction}.\n\n### Input:\n{input}\n"
+    #     "(1) Undesired one: {output} \n(2) Desired one: {cleaned_output} \n\n### Response:\n"
+    # ),
+    # "instruct_output_contrast": (
+    #     "{output}"
+    # ),
+
+    # Above version deprecated.
+    # Aligning prompts between training and evaluation of contrast strategy.
     "instruct_tuning_contrast": (
-        "Below is a question and related responses: a desired one and an undesired one. The undesired and desired examples will not be given during inference.\n\n"
-        "### Instruction:\n{instruction}.\n\n### Input:\n{input}\n"
-        "(1) Undesired one: {output} \n(2) Desired one: {cleaned_output} \n\n### Response:\n"
+        "Below is a question and related responses. "
+        "Write \n(1) desired answer to the question. \n(2) undesired answer to the question. \n\n"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
     ),
     "instruct_output_contrast": (
-        "{output}"
+        "(1) desired answer to the question: {output}\n(2) undesired answer to the question: {cleaned_output}\n"
     ),
 }
--- inference/pl.py
+++ inference/pl.py
@@ -15,15 +15,15 @@ parser.add_argument('--template', required=True, help='Template name')
 args = parser.parse_args()
 dataset_name = args.dataset # [wikidoc flashcards]
 template_name = args.template # [original instruct contrast]
+ML = 384
+BS = 64
 
-generator = pipeline(task='text-generation', model=f"/home/dsi/yxiao/plm/ckpt/merge/7B-{template_name}-{dataset_name}", device_map="auto", batch_size=32, max_new_tokens=512)
+generator = pipeline(task='text-generation', model=f"/home/dsi/yxiao/plm/ckpt/merge/{template_name}-{dataset_name}-7B-{ML}-{BS}", device_map="auto", batch_size=32, max_new_tokens=512)
 
-    generator.tokenizer.add_special_tokens(
-    {
+generator.tokenizer.add_special_tokens({
         "pad_token": "<PAD>"
     }
 )
-
 generator.model.resize_token_embeddings(generator.model.config.vocab_size + 1)
 
 batched_prompts = json.load(open(f'./inference/examples/{dataset_name}-{template_name}.json', 'r'))
